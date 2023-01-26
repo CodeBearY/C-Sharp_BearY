4e9f7f047e923636b5c9c2faf386fe6801ccf37f
Bernhard Siegl <sie19038@spengergasse.at>
2023-01-26 16:46:47 +0100

    Adds Domain Model - Planets - Galaxys

---
+++ MachineLearning_Backend/Infrastructure/Domain/Model/Planet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineLearning_Backend.Infrastructure.Domain.Model
+{
+    class Planet
+    {
+        //[Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public float? Temperature { get; set; }
+
+    }
+}
